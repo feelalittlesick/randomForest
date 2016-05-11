3c85bee4053457d605961b8e1d1f4c34a50020db
zaq12wsx <feelalittleskic@gmail.com>
2016-05-11 19:51:42 +0200

    Kons

--- rand.py
+++ rand.py
@@ -1,21 +1,19 @@
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-import numpy as np
+from numpy import genfromtxt, savetxt
 
-iris = load_iris()
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
-df['species'] = pd.Factor(iris.target, iris.target_names)
-df.head()
+def main():
+    #create the training & test sets, skipping the header row with [1:]
+    dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
+    target = [x[0] for x in dataset]
+    train = [x[1:] for x in dataset]
+    test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+    
+    #create and train the random forest
+    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    rf = RandomForestClassifier(n_estimators=100)
+    rf.fit(train, target)
 
-train, test = df[df['is_train']==True], df[df['is_train']==False]
+    savetxt('Data/submission2.csv', rf.predict(test), delimiter=',', fmt='%f')
 
-features = df.columns[:4]
-clf = RandomForestClassifier(n_jobs=2)
-y, _ = pd.factorize(train['species'])
-clf.fit(train[features], y)
-
-preds = iris.target_names[clf.predict(test[features])]
-pd.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
-pd.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
+if __name__=="__main__":
+    main()
