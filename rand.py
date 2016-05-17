9a7392a4a227da670b46467597d17e9b108b260f
zaq12wsx <feelalittleskic@gmail.com>
2016-05-18 01:59:04 +0200

    Program przystosowany pod nowe dane

--- rand.py
+++ rand.py
@@ -1,19 +1,27 @@
+print('Importing libraries...')
+import numpy as np
+import pandas as pd
+from sklearn import cross_validation as cv
+from sklearn.cross_validation import KFold
 from sklearn.ensemble import RandomForestClassifier
-from numpy import genfromtxt, savetxt
 
-def main():
-    #create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
-    target = [x[0] for x in dataset]
-    train = [x[1:] for x in dataset]
-    test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
-    
-    #create and train the random forest
-    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
-    rf = RandomForestClassifier(n_estimators=100)
-    rf.fit(train, target)
+train = pd.read_csv("Data/cancerTrain.csv", delimiter=';', )
+test  = pd.read_csv("Data/cancerTest.csv", delimiter=';', )
+train.info()
 
-    savetxt('Data/submission2.csv', rf.predict(test), delimiter=',', fmt='%f')
+print('Defining submission file...')    
+def create_submission(rfc, train, test, predictors, filename):
+    rfc.fit(train[predictors], train["Class"])
+    predictions = rfc.predict(test[predictors])
+    submission = pd.DataFrame({
+        "PatientId": test["PatientId"],
+        "Class": predictions
+    })
+    submission.to_csv(filename, index=False)
 
-if __name__=="__main__":
-    main()
+predictors = ["ClumpThickness", "UniformityCellSize", "CellShape", "MarginalAdhesion", "CellSize", "BareNuclei", "BlandChromatin", "NormalNucleoli", "Mitoses"]
+
+rfc = RandomForestClassifier(n_estimators=100)
+print('Creating submission...')
+create_submission(rfc, train, test, predictors, "predictions.csv")
+print('Submitted.')
